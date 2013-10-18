8e49a470a2ed004092090a2ac585d0a82766c2ab
Shaun Burdick <shaun.burdick@twcable.com>
2013-10-18 10:39:30 -0400

    Added some parameter testing

--- rest.go
+++ rest.go
@@ -5,6 +5,7 @@ import (
   "net/http"
   "log"
   "fmt"
+  "strings"
   "github.com/gorilla/mux"
 )
 
@@ -21,9 +22,28 @@ func main() {
 }
 
 func HomeHandler(w http.ResponseWriter, req *http.Request) {
-  s := "You hit %s from %s via %s method\n"
-  io.WriteString(w, fmt.Sprintf(s, 
-                                req.URL.String(), 
-                                req.Host, 
-                                req.Method))
+  err := req.ParseMultipartForm(1000)
+  if err != nil {
+    log.Fatal("HomeHandler: ", err)
+  }
+
+  format := " hit %s%s from %s via %s method\n"
+  s := fmt.Sprintf(format, 
+                   req.Host,
+                   req.URL.String(), 
+                   req.RemoteAddr, 
+                   req.Method)
+
+  io.WriteString(w, "You " + s)
+  fmt.Print("They " + s)
+
+  if len(req.Form) != 0 {
+    io.WriteString(w, "\nWith the following data:\n")
+    fmt.Println("\nWith the following data:")
+    for param, val := range req.Form {
+      io.WriteString(w, "-" + param + ": " + strings.Join(val, ", ") + "\n")
+      fmt.Println("-" + param + ": " + strings.Join(val, ", "))
+    }
+    fmt.Print("\n\n=====================\n\n")
+  }
 }
